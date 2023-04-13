648e610e924a056a4a93fa0678a08abdd100a4f9
andreidrr <andrei.dr@hotmail.com>
2023-04-13 10:10:37 -0300

    terraform

---
+++ main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+#digital_droplet criando a maquina e passando os parametros
+resource "digitalocean_droplet" "jenkies" {
+  image  = "ubuntu-22-04-x64"
+  name   = "jenkies"
+  region = var.region
+  size   = "s-2vcpu-2gb"
+  #vinculando a chave ssh
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
+}
+
+
+data "digitalocean_ssh_key" "ssh_key" {
+  name = var.ssh_key_name
+}
+
+
+#Criando o Cluster kubernets:
+
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name   = "k8s"
+  region = var.region
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.26.3-do.0"
+  node_pool {
+    name       = "default"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+  }
+}
+
+
+#Criando variaveis
+variable "do_token" {
+  default = ""
+}
+
+variable "ssh_key_name" {
+  default = ""
+}
+
+variable "region" {
+  default = ""
+}
+
+output "jenkies_ip" {
+  value       = digitalocean_droplet.jenkies.ipv4_address
+}
+
+resource "local_file" "foo" {
+  content  = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
+  filename = "kube_config.yaml"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+do_token     = "dop_v1_f5579e4d3c7c891a0038a4e168b88b885043aba19882949bc6b52f40d5fdf47c"
+ssh_key_name = "Jornada"
+region       = "nyc3"
